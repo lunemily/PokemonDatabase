b1ebb0b687add80b01d1cecf67cdf4c4ccfb84de
Evan Fuhr <evanfuhr@gmail.com>
2016-08-03 13:17:18 -0500

    Update Pokemon.java
    Refactored Pokemon to use Type objects instead of simple ids

--- app/src/main/java/com/evanfuhr/pokemondatabase/Pokemon.java
+++ app/src/main/java/com/evanfuhr/pokemondatabase/Pokemon.java
@@ -1,14 +1,12 @@
 package com.evanfuhr.pokemondatabase;
 
-/**
- * Created by Evan on 30-Jul-16.
- */
 public class Pokemon {
 
     private int _id;
     private String _name;
     private int _type_1;
     private int _type_2;
+    private List<Type> _types;
 
     public int getID() {
         return this._id;
@@ -26,19 +24,31 @@ public class Pokemon {
         this._name = name;
     }
 
+    @Deprecated
     public int getType1() {
         return this._type_1;
     }
 
+    @Deprecated
     public void setType1(int type_id) {
         this._type_1 = type_id;
     }
 
+    @Deprecated
     public int getType2() {
         return this._type_2;
     }
 
+    @Deprecated
     public void setType2(int type_id) {
         this._type_2 = type_id;
+    }
+    
+    public List<Type> getTypes() {
+        return this._types;
+    }
+    
+    public void setTypes(List<Type> types) {
+        this._types = types;
     }
 }
