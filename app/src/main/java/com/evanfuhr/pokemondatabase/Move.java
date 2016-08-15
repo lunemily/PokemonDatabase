3b7469094f87e711c7d3774d89f37fd6bcc80765
Evan Fuhr <evanfuhr@gmail.com>
2016-08-15 10:20:02 -0500

    Update Move.java
    Added getters and setters for accuracy and pp

--- app/src/main/java/com/evanfuhr/pokemondatabase/Move.java
+++ app/src/main/java/com/evanfuhr/pokemondatabase/Move.java
@@ -8,46 +8,31 @@ import java.util.List;
  */
 public class Move {
 
+    private int _accuracy;
     private int _id;
     private int _level = 0;
     private int _method_id;
     private String _name;
     private int _power = 0;
+    private int _pp;
     private Type _type;
 
+    
+
+    public int getAccuracy() {
+        return this._accuracy;
+    }
+
+    public void setAccuracy(int accuracy) {
+        this._accuracy = accuracy;
+    }
+    
     public int getID() {
         return this._id;
     }
 
     public void setID(int id) {
         this._id = id;
-    }
-
-    public String getName() {
-        return this._name;
-    }
-
-    public void setName(String name) {
-        this._name = name;
-    }
-
-    public Type getType() {
-        return this._type;
-    }
-
-    //TODO: Convert to type object
-    public void setType(Type type) {
-        this._type = type;
-    }
-
-
-
-    public int getMethodID() {
-        return this._method_id;
-    }
-
-    public void setMethodID(int method_id) {
-        this._method_id = method_id;
     }
 
     public int getLevel() {
@@ -58,12 +43,44 @@ public class Move {
         this._level = level;
     }
 
+    public int getMethodID() {
+        return this._method_id;
+    }
+
+    public void setMethodID(int method_id) {
+        this._method_id = method_id;
+    }
+
+    public String getName() {
+        return this._name;
+    }
+
+    public void setName(String name) {
+        this._name = name;
+    }
+
     public int getPower() {
         return this._power;
     }
 
     public void setPower(int power) {
         this._power = power;
+    }
+
+    public int getPP() {
+        return this._pp;
+    }
+
+    public void setPP(int pp) {
+        this._pp = pp;
+    }
+
+    public Type getType() {
+        return this._type;
+    }
+
+    public void setType(Type type) {
+        this._type = type;
     }
 
     //Static methods
