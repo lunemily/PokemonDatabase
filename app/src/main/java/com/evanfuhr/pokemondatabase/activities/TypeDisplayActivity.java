f55458f5378fabfd520b76f4bcca9300b827b420
evanfuhr <evanfuhr@gmail.com>
2017-09-30 12:45:43 -0500

    Just barely got recycler to show up in non-list activity

--- app/src/main/java/com/evanfuhr/pokemondatabase/activities/TypeDisplayActivity.java
+++ app/src/main/java/com/evanfuhr/pokemondatabase/activities/TypeDisplayActivity.java
@@ -9,26 +9,37 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.evanfuhr.pokemondatabase.R;
 import com.evanfuhr.pokemondatabase.data.TypeDAO;
+import com.evanfuhr.pokemondatabase.fragments.PokemonListFragment;
 import com.evanfuhr.pokemondatabase.fragments.TypeMatchUpFragment;
 import com.evanfuhr.pokemondatabase.interfaces.OnTypeSelectedListener;
+import com.evanfuhr.pokemondatabase.models.Pokemon;
 import com.evanfuhr.pokemondatabase.models.Type;
 
 public class TypeDisplayActivity extends AppCompatActivity
-        implements OnTypeSelectedListener {
+        implements OnTypeSelectedListener, PokemonListFragment.OnListFragmentInteractionListener {
 
     Type _type = new Type();
+    public final String TYPE_ID = "type_id";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_type_display);
-
-        TypeDAO typeDAO = new TypeDAO(this);
 
         //Get pokemon id passed to this activity
         Intent intent = getIntent();
         _type.setID(intent.getIntExtra(TypeListActivity.TYPE_ID, 0));
+
+        Bundle bundle = new Bundle();
+        bundle.putInt(TYPE_ID, _type.getID());
+// set Fragmentclass Arguments
+        PokemonListFragment fragobj = new PokemonListFragment();
+        fragobj.setArguments(bundle);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_type_display);
+
+        TypeDAO typeDAO = new TypeDAO(this);
         _type = typeDAO.getTypeByID(_type);
+
         onTypeSelected(_type);
         setTitle(_type.getName());
     }
@@ -48,5 +59,10 @@ public class TypeDisplayActivity extends AppCompatActivity
         TypeDAO typeDAO = new TypeDAO(this);
         ConstraintLayout typeDisplayActivity = (ConstraintLayout) findViewById(R.id.type_display_activity);
         typeDisplayActivity.setBackgroundColor(Color.parseColor(typeDAO.getTypeByID(type).getColor()));
+    }
+
+    @Override
+    public void onListFragmentInteraction(Pokemon item) {
+
     }
 }
