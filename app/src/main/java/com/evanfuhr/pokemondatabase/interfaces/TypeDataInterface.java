c3651b85ab5493335d8f1202c9a1afa7757a5c29
EVAN-GAMING-WIN\evanf <evanfuhr@gmail.com>
2018-02-11 18:06:45 -0600

    Fixed imports.

--- app/src/main/java/com/evanfuhr/pokemondatabase/interfaces/TypeDataInterface.java
+++ app/src/main/java/com/evanfuhr/pokemondatabase/interfaces/TypeDataInterface.java
@@ -1,12 +1,7 @@
 package com.evanfuhr.pokemondatabase.interfaces;
 
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
-
 import com.evanfuhr.pokemondatabase.models.Type;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public interface TypeDataInterface {
