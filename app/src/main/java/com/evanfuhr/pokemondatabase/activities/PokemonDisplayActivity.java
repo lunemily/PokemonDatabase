e39fe4c9becc4f1954c4849325ef93df0e568825
Evan Fuhr <evanfuhr@gmail.com>
2018-05-20 23:37:46 -0500

    Anonymous VersionManager now. Cleaned imports

--- app/src/main/java/com/evanfuhr/pokemondatabase/activities/PokemonDisplayActivity.java
+++ app/src/main/java/com/evanfuhr/pokemondatabase/activities/PokemonDisplayActivity.java
@@ -1,30 +1,18 @@
 package com.evanfuhr.pokemondatabase.activities;
 
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.GestureDetector;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.RelativeLayout;
-import android.widget.Spinner;
 
 import com.evanfuhr.pokemondatabase.R;
-import com.evanfuhr.pokemondatabase.adapters.VersionSpinnerAdapter;
 import com.evanfuhr.pokemondatabase.data.PokemonDAO;
 import com.evanfuhr.pokemondatabase.data.TypeDAO;
-import com.evanfuhr.pokemondatabase.data.VersionDAO;
 import com.evanfuhr.pokemondatabase.fragments.AbilityListFragment;
 import com.evanfuhr.pokemondatabase.fragments.LocationListFragment;
 import com.evanfuhr.pokemondatabase.fragments.MoveListFragment;
@@ -35,7 +23,6 @@ import com.evanfuhr.pokemondatabase.models.Location;
 import com.evanfuhr.pokemondatabase.models.Move;
 import com.evanfuhr.pokemondatabase.models.Pokemon;
 import com.evanfuhr.pokemondatabase.models.Type;
-import com.evanfuhr.pokemondatabase.models.Version;
 import com.evanfuhr.pokemondatabase.utils.PokemonUtils;
 import com.evanfuhr.pokemondatabase.utils.VersionManager;
 
@@ -51,14 +38,10 @@ public class PokemonDisplayActivity extends AppCompatActivity
 
     @NonNls
     public static final String POKEMON_ID = "pokemon_id";
-    public static final String ANIMATION = "animation";
 
     RelativeLayout mRelativeLayout;
 
-    public Pokemon pokemon = new Pokemon();
-    VersionManager mVersionManager;
-
-    private GestureDetector mDetector;
+    Pokemon mPokemon = new Pokemon();
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
@@ -66,58 +49,27 @@ public class PokemonDisplayActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pokemon_display);
 
-        PokemonDAO pokemonDAO = new PokemonDAO(this);
-
         mRelativeLayout = findViewById(R.id.pokemon_display_activity);
-        mVersionManager = new VersionManager(this);
 
+        loadPokemon();
+        setBackgroundColor();
+        setTitle("#" + mPokemon.getId() + " " + mPokemon.getName());
+    }
+
+    private void loadPokemon() {
+        PokemonDAO pokemonDAO = new PokemonDAO(this);
         //Get pokemon id passed to this activity
         Intent intent = getIntent();
-        pokemon.setId(intent.getIntExtra(POKEMON_ID, 0));
-        pokemon = pokemonDAO.getPokemon(pokemon);
-        setPokemonBackgroundColor(pokemon);
-        setTitle("#" + pokemon.getId() + " " + pokemon.getName());
-
-        mDetector = new GestureDetector(this, new MyGestureListener(pokemon.getId(), this));
-
+        mPokemon.setId(intent.getIntExtra(POKEMON_ID, 0));
+        mPokemon = pokemonDAO.getPokemon(mPokemon);
         pokemonDAO.close();
-
-        if (intent.getExtras().containsKey(ANIMATION)) {
-            switch (intent.getIntExtra(ANIMATION, 0)) {
-                case R.anim.trans_left_in:
-                    overridePendingTransition(R.anim.trans_left_in, R.anim.trans_right_out);
-                    break;
-                case R.anim.trans_right_in:
-                    overridePendingTransition(R.anim.trans_right_in, R.anim.trans_left_out);
-                    break;
-                default:
-                    overridePendingTransition(R.anim.trans_right_in, R.anim.trans_left_out);
-                    break;
-            }
-        }
     }
 
-    @Override
-    public boolean dispatchTouchEvent(MotionEvent ev) {
-        boolean handled = mDetector.onTouchEvent(ev);
-        if (!handled) {
-            return super.dispatchTouchEvent(ev);
-        }
-
-        return handled;
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        this.mDetector.onTouchEvent(event);
-        return super.onTouchEvent(event);
-    }
-
-    private void setPokemonBackgroundColor(Pokemon pokemon) {
+    private void setBackgroundColor() {
         TypeDAO typeDAO = new TypeDAO(this);
 
         //Create base background
-        List<Type> rawTypes = typeDAO.getTypes(pokemon);
+        List<Type> rawTypes = typeDAO.getTypes(mPokemon);
         List<Type> types = new ArrayList<>();
         for (Type t : rawTypes) {
             types.add(typeDAO.getType(t));
@@ -147,7 +99,7 @@ public class PokemonDisplayActivity extends AppCompatActivity
 
         switch(id) {
             case R.id.action_set_game:
-                mVersionManager.onClickMenuSetGame();
+                new VersionManager(this).onClickMenuSetGame();
                 break;
             case R.id.action_search_list:
                 break;
@@ -193,61 +145,12 @@ public class PokemonDisplayActivity extends AppCompatActivity
 
     @Override
     public void onListFragmentInteraction(Location location) {
+        PokemonUtils.showLoadingToast(this);
+        // Build the intent to load the ability display
+        Intent intent = new Intent(this, LocationDisplayActivity.class);
+        // Load the ability id into the intent
+        intent.putExtra(LocationDisplayActivity.LOCATION_ID, location.getId());
 
-    }
-
-    private class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
-        private static final String DEBUG_TAG = "Gestures";
-        private int mCurrentPokemonID = 0;
-        private float deltaX, deltaY;
-        Context context;
-
-        public MyGestureListener(int currentPokemonID, Context context) {
-            this.context = context;
-            this.mCurrentPokemonID = currentPokemonID;
-        }
-
-        @Override
-        public boolean onDown(MotionEvent event) {
-            Log.d(DEBUG_TAG, "onDown: " + event.toString());
-            return false;
-        }
-
-        @Override
-        public boolean onFling(MotionEvent event1, MotionEvent event2,
-                               float velocityX, float velocityY) {
-
-            deltaX = event2.getX() - event1.getX();
-            deltaY = event2.getY() - event1.getY();
-
-            Log.d(DEBUG_TAG, "onFling: " + event1.toString() + event2.toString());
-
-            if (Math.abs(deltaY) > 4 * Math.abs(deltaX)) { // If vertical fling, just scroll
-                return false;
-            } else { // Go to adjacent pokemon
-                PokemonUtils.showLoadingToast(this.context);
-                Intent intent = new Intent(getApplicationContext(), PokemonDisplayActivity.class);
-                boolean swipeRight = (deltaX < 0);
-                int newPokemonID;
-                int animation;
-
-                if (swipeRight) {
-                    //TODO: Don't hardcode this
-                    newPokemonID = (mCurrentPokemonID + 1 <= 802) ? mCurrentPokemonID + 1 : 1; // Wrap around
-                    animation = R.anim.trans_left_in;
-                } else { //swipeLeft
-                    //TODO: Don't hardcode this
-                    newPokemonID = (mCurrentPokemonID - 1 > 0) ? mCurrentPokemonID - 1 : 802; // Wrap around
-                    animation = R.anim.trans_right_in;
-                }
-
-                intent.putExtra(POKEMON_ID, newPokemonID);
-                intent.putExtra(ANIMATION, animation);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-                startActivity(intent);
-                return false;
-            }
-        }
+        startActivity(intent);
     }
 }
